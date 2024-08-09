0934d3d2e01ed36efd8cc30a0b3e17e1c6d43e12
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2024-08-09 07:18:49 +0100

    feat: js logic for displaying movies from OMDB API

--- js/script.js
+++ js/script.js
@@ -1,54 +1,60 @@
 const moviesEl = document.getElementById('movies')
 const searchbarBtn = document.getElementById('searchbar-btn')
 const searchbarInput = document.getElementById('searchbar-btn')
+let moviesHtml = ''
 
-// fetch("http://www.omdbapi.com/?apikey=d30c0673&s=blade+runner")
-//   .then(response => response.json())
-//   .then(data => {
-//     getMovieCards(data.Search)
-//   })
-""
-fetch("http://www.omdbapi.com/?apikey=d30c0673&i=tt0083658")
+// async function handleClick(){}
+// get search results from api call 
+fetch("http://www.omdbapi.com/?apikey=d30c0673&s=blade+runner")
   .then(response => response.json())
-  .then(data => {
-    getMovieCards(data)
+  .then(searchData => {
+    getMovieData(searchData.Search)
   })
 
-function getMovieCards(movie) {
-  console.log(movie)
+// get movie data for each movie
+async function getMovieData(moviesList) {
+  for (let movie of moviesList) {
+    await displayMovieCards(movie)
+  }
+  render()
+}
 
-  let moviesHtml = ''
+// display movie cards from data 
+async function displayMovieCards(movieData) {
+  const response = await fetch(`http://www.omdbapi.com/?apikey=d30c0673&i=${movieData.imdbID}`)
+  const movie = await response.json()
 
-  // movieData.forEach(movie => {
-    let movieRating = movie.Ratings[0].Value.substring(
-      0, movie.Ratings[0].Value.length - 3
-    )
+  let movieRating = movie.Ratings[0].Value.substring(
+    0, movie.Ratings[0].Value.length - 3
+  )
 
-    moviesHtml += `
-      <article class="movie-card">
-        <img class="movie-img" src=${movie.Poster}>
-        <div class="movie-text">
-          <div class="movie-text-header">
-            <h2 class="movie-title">${movie.Title}</h2>
-            <i class="fa-solid fa-star fa-xs star"></i>
-            <p class="movie-rating">${movieRating}</p>
-          </div>
-          <div class="movie-text-info">
-            <p class="movie-time">${movie.Runtime}</p>
-            <p class="movie-genre">${movie.Genre}</p>
-            <button class="movie-add-to-watchlist-btn">
-              <i class="fa-solid fa-circle-plus fa-lg plus-btn"></i>
-              Watchlist
-            </button>
-          </div>
-          <p class="movie-description">${movie.Plot}</p>
+  moviesHtml += `
+    <article class="movie-card">
+      <img class="movie-img" src=${movie.Poster}>
+      <div class="movie-text">
+        <div class="movie-text-header">
+          <h2 class="movie-title">${movie.Title}</h2>
+          <i class="fa-solid fa-star fa-xs star"></i>
+          <p class="movie-rating">${movieRating}</p>
         </div>
-      </article>
-    `
-  // })
+        <div class="movie-text-info">
+          <p class="movie-time">${movie.Runtime}</p>
+          <p class="movie-genre">${movie.Genre}</p>
+          <button class="movie-add-to-watchlist-btn">
+            <i class="fa-solid fa-circle-plus fa-lg plus-btn"></i>
+            Watchlist
+          </button>
+        </div>
+        <p class="movie-description">${movie.Plot}</p>
+      </div>
+    </article>
+  `
+}
+
+// render dynamic html onto screen 
+function render() {
   moviesEl.innerHTML = moviesHtml
 }
-console.log(moviesEl)
 
 
 /* 
@@ -65,4 +71,12 @@ To Do:
   add to watchlist btn 
   watchlist page 
   remove from watchlist btn 
+*/
+
+
+/* Learning Notes: 
+lines 13-18 async function getMovieData() {}
+  forEach() doesn't work to make the fetch calls inside loop synchronous. idk why. but it works with for loop, doesn't work with forEach(). 
+  have to use async here because I need to use await before calling getMovieCards() so it gets each synchronously (previously it would do it async and it would load each movie in a different random order whereas we want it in the same order each time someone searches for that exact same result)
+
 */ 
